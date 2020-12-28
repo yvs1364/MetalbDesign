50866f06e67bcbe60170656f9e42c63c9ab54f72
yvs1364 <yvs.pro64@gmail.com>
2020-12-28 13:29:56 +0100

    add asset false

--- config/application.rb
+++ config/application.rb
@@ -12,10 +12,11 @@ module BastienWebsite
     config.load_defaults 6.0
     config.assets.enabled = false
     config.action_view.embed_authenticity_token_in_remote_forms = true
-    
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
 end
+
