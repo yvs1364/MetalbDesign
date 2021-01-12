5a963095b5da4d52f5235f73535d785f7aedd99c
yvs1364 <yvs.pro64@gmail.com>
2021-01-12 21:30:42 +0100

    add auto copyright year

--- app/javascript/packs/dist/application.dev.js
+++ app/javascript/packs/dist/application.dev.js
@@ -1,15 +1,16 @@
+"use strict";
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+require("@rails/ujs").start();
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("turbolinks").start();
 
+require("@rails/activestorage").start();
 
-// Uncomment to copy all static images under ../images to the output folder and reference
+require("channels"); // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
@@ -21,8 +22,9 @@ require("channels")
 //= require jquery.mobile
 //= require turbolinks
 
-require("trix")
-require("@rails/actiontext")
 
+require("trix");
+
+require("@rails/actiontext");
 
 console.log("Hello from app/javascript/packs/application.js!");
--- config/webpack/dist/development.dev.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-var webpackConfig = require('./base');
-
-module.exports = webpackConfig;
